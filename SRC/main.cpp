58fb17cb5f396cc5c0fff69c477a77f2082e8e5c
J-G-12 <JAVIER.GOMEZ-G@mail.escuelaing.edu.co>
2025-12-09 22:53:13 -0500

    Update main.cpp

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,1 +1,12 @@
+#include "mainwindow.h"
+#include <QApplication>
 
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    app.setApplicationName("Gestor de Inventario IoT");
+    
+    MainWindow window;
+    window.show();
+    
+    return app.exec();
+}
